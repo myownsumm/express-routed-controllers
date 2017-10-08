009b2be534b4c447998d0dc659d702e3cefc7e3f
alexander.poltoratskiy <myownsumm@gmail.com>
2017-10-08 12:47:27 +0300

    remove test route

--- router.js
+++ router.js
@@ -4,13 +4,6 @@ const router = express.Router({
     mergeParams: true
 });
 
-router.get('/test-lib', (req, res, next) => {
-    return res.json({
-        result: true,
-        message: 'hello'
-    })
-});
-
 class DynamicRouter {
     constructor() {
         this._router = router;
