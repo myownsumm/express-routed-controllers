3af660285906eff6dfc44490a4f291ffa4bc53cf
alexander.poltoratskiy <myownsumm@gmail.com>
2017-10-15 17:56:21 +0300

    base types added

---
+++ types.d.ts
@@ -0,0 +1,19 @@
+// Type definitions for express-routed-controllers
+// Project: https://github.com/myownsumm/express-routed-controllers
+// Definitions by: Alexander Poltoratskiy <https://github.com/myownsumm>
+// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
+
+
+declare module "express-routed-controllers" {
+    export class BaseController {
+        response(data: object, format?: string);
+    }
+
+    export class DynamicRouter {
+
+    }
+}
+
+
+
+
