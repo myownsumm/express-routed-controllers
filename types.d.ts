bf8495c5ba27e5ff40f816aff3147229a1621fbd
alexander.poltoratskiy <myownsumm@gmail.com>
2017-11-02 20:53:16 +0200

    no need in these comments

--- types.d.ts
+++ types.d.ts
@@ -1,9 +1,3 @@
-// Type definitions for express-routed-controllers
-// Project: https://github.com/myownsumm/express-routed-controllers
-// Definitions by: Alexander Poltoratskiy <https://github.com/myownsumm>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
-
 declare module "express-routed-controllers" {
     import * as express from 'express';
 
