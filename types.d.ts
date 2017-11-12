669e432f7e1cc651dbaea94e2463ff1602e100e9
alexander.poltoratskiy <myownsumm@gmail.com>
2017-11-12 15:51:43 +0200

    add other available methods typings

--- types.d.ts
+++ types.d.ts
@@ -11,6 +11,10 @@ declare module "express-routed-controllers" {
     export class DynamicRouter {
         // todo find way to specify controller type of RoutedController
         controller(action: string, controller: any);
+        get(action: string, controller: any, method: string);
+        put(action: string, controller: any, method: string);
+        post(action: string, controller: any, method: string);
+        delete(action: string, controller: any, method: string);
 
         public group(params: { prefix?: string, middlewares?: ((req: Express.Request, res: Express.Response, next) => any)[] }, callback);
 
