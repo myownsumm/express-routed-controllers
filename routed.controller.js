6444a8c5f2c5a845ce7780f8492bb704e1756741
alexander.poltoratskiy <myownsumm@gmail.com>
2018-04-10 16:39:07 +0300

    converting funcs added, but not enabled

--- routed.controller.js
+++ routed.controller.js
@@ -1,6 +1,7 @@
 class RoutedController {
     constructor(req, res) {
         this._req = req;
+        // this._req = this._convertRequestProps(req);
         this._res = res;
     }
 
@@ -15,6 +16,45 @@ class RoutedController {
     get response() {
         return this._res;
     }
+
+    // _convertRequestProps(request) {
+    //     let requestProps = ['body', 'params', 'query'];
+    //
+    //     requestProps.forEach(requestProperty => {
+    //         if (!request[requestProperty]) {
+    //             return false;
+    //         }
+    //
+    //         for (let prop in request[requestProperty]) {
+    //             if (!request[requestProperty].hasOwnProperty(prop)) {
+    //                 continue;
+    //             }
+    //
+    //             this._convertToBooleanPropIfRequired(prop);
+    //             this._convertToIntegerPropIfRequired(prop);
+    //         }
+    //     });
+    //
+    //     return requestProps;
+    // }
+    //
+    // _convertToBooleanPropIfRequired(propertyValue) {
+    //     if (propertyValue === 'true' || propertyValue === 'false') {
+    //         propertyValue = propertyValue === 'true';
+    //     }
+    //
+    //     console.log('propertyValue', propertyValue);
+    //
+    //     return propertyValue;
+    // }
+    //
+    // _convertToIntegerPropIfRequired(propertyValue) {
+    //     if (/^-?([0-9]*[.])?[0-9]+$/.test(propertyValue)) {
+    //         propertyValue = Number(propertyValue);
+    //     }
+    //
+    //     return propertyValue;
+    // }
 }
 
 module.exports = RoutedController;
